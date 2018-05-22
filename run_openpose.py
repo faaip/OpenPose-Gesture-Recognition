2ac9b6a757d3db9a07591a21961ed3d869ae41e6
mxmxxl <m.vanoldenbeek@gmail.com>
2018-05-22 14:35:45 +0200

    more exceptions

--- run_openpose.py
+++ run_openpose.py
@@ -7,11 +7,8 @@ import pandas as pd
 import json
 import argparse
 from scripts.format_json import json_join
+import sys
 
-parser = argparse.ArgumentParser(description='Process arguments.')
-parser.add_argument('--type', type=str, help='train|val|test', default = 'train')
-
-ds_type = parser.parse_args().type
 
 def get_now():
     return datetime.datetime.now().strftime('%y-%m-%d_%H%M%S')
@@ -20,11 +17,14 @@ def write_log(log_name, message, log_type = 'INFO'):
     with open(log_name, 'a') as f:
         f.write('[{0}] [{2}]: {1}\n'.format(get_now(),message, log_type))
 
+def shutdown(message = 'Shutting down machine'):
+    sys.exit(message)
+
 vid_dir = 'images'
 json_dir = 'json'
 log_dir = 'logs'
 label_dir = 'labels'
-
+script_dir = 'scripts'
 
 
 # The the absolute directory path of where the python file is located
@@ -35,32 +35,49 @@ vid_dir = join(root,vid_dir)
 json_dir = join(root,json_dir)
 log_dir = join(root,log_dir)
 label_dir = join(root,label_dir)
+script_dir = join(root,script_dir)
 
-log = join(log_dir,get_now()+'.txt')
+try: 
+    log = join(log_dir,get_now()+'.txt')
+    write_log(log,'Starting logging for pid {}'.format(os.getpid()))
 
-
-json_file = join(label_dir,'train.csv')
-labels = pd.read_csv(json_file, index_col = 0, sep = ';', header = None)
-labels = json.loads(labels.to_json())['1']
-
-json_file = join(label_dir,'validation.csv')
-validation_labels = pd.read_csv(json_file, index_col = 0, sep = ';', header = None)
-validation_labels = json.loads(validation_labels.to_json())['1']
+except BaseException as e:
+    shutdown()
 
 
-json_file = join(label_dir,'test.csv')
-test_labels = pd.read_csv(json_file, index_col = 0, sep = ';', header = None)
-test_labels['1'] = '?'
-test_labels = json.loads(test_labels.to_json())['1']
+try:
+    json_file = join(label_dir,'train.csv')
+    labels = pd.read_csv(json_file, index_col = 0, sep = ';', header = None)
+    labels = json.loads(labels.to_json())['1']
+
+    json_file = join(label_dir,'validation.csv')
+    validation_labels = pd.read_csv(json_file, index_col = 0, sep = ';', header = None)
+    validation_labels = json.loads(validation_labels.to_json())['1']
 
 
-labels.update(validation_labels)
-labels.update(test_labels)
+    json_file = join(label_dir,'test.csv')
+    test_labels = pd.read_csv(json_file, index_col = 0, sep = ';', header = None)
+    test_labels['1'] = '?'
+    test_labels = json.loads(test_labels.to_json())['1']
+
+
+    labels.update(validation_labels)
+    labels.update(test_labels)
+
+except BaseException as e:
+    write_log(log,'Unable to get labels: {}'.format(e),log_type = 'FATAL')
+    shutdown()
+
+
 
 # Get the video directories only as directory name
-vid_list = [i for i in os.listdir(vid_dir) if isdir(join(vid_dir,i)) ] 
+try:
+    vid_list = [i for i in os.listdir(vid_dir) if isdir(join(vid_dir,i)) ] 
+    vid_list = np.random.permutation(vid_list)
+except BaseException as e:
+    write_log(log,'Cannot get video lists', log_type = 'FATAL')
+    shutdown()
 
-vid_list = np.random.permutation(vid_list)
 
 
 for j, i in enumerate(vid_list):
@@ -73,15 +90,18 @@ for j, i in enumerate(vid_list):
 
     # if the directory exists then we can skip it 
     # In case we run need to run it twice.
+    try:
+        if isfile(big_json_name):
+            write_log(log,'Big json exists going to next video')
+            continue
+        else:
+            write_log(log,'Making json directory')
+            os.makedirs(output_dir) 
     
-    if isfile(big_json_name):
-        write_log(log,'Big json exists going to next video')
+    except BaseException as e:
+        write_log(log,'Something wrong with json file/dir: {}'.format(e),log_type = 'ERROR')
         continue
-    else:
-        write_log(log,'Making json directory')
-        os.makedirs(output_dir) 
-    
-    
+
     ### run the openpose command ###
     write_log(log,'Extracting features using openPose')
     rc = subprocess.run(['./scripts/openpose.sh','{}'.format(input_dir),'{}'.format(output_dir) ], stdout=subprocess.PIPE, stderr = subprocess.PIPE )
