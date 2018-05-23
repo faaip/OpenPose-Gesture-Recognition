28eb448b71e367d45ba542d7248650cff960a318
Ubuntu <ubuntu@ip-172-31-22-40.us-west-2.compute.internal>
2018-05-23 09:46:47 +0000

    maxime forgot something

--- scripts/copy_stuff.sh
+++ scripts/copy_stuff.sh
@@ -1,6 +1,6 @@
-scrdir=./train/
-outdir=./recog_test
+scrdir=/home/ubuntu/20bn-datasets/20bn-jester-v1
+outdir=/home/ubuntu/data/OpenPose-Gesture-Recognition/images
 N=50
 
 for i in $( ls $scrdir | head -n $N ); do
-	cp $scrdir/$i $outdir; done
+	cp -r $scrdir/$i $outdir; done
